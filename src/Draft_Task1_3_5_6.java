1ae7728cc66c8fc48af68218eb4fcda7b8e18c63
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-24 14:37:15 +0300

    Initial commit

--- src/Draft_Task1_3_5_6.java
+++ src/Draft_Task1_3_5_6.java
@@ -1,10 +1,47 @@
 import java.util.Scanner;
 
-import static java.lang.Math.min;
-//import static java.lang.StringUTF16.charAt;
-//import static sun.util.calendar.ZoneInfoFile.indexOf;
 
-////        Пример _ ППППППППППППППППППППППППППППППППППП
+//        Пример _ ППППППППППППППППППППППППППППППППППП СДЕЛАН ОКОНЧАТЕЛЬНО, ОТПРАВЛЕН В ЭТОМ ВИДЕ !!!!!
+//  Пример взят отсюда: https://gist.github.com/ezhov-da/1544cb56555f6319f5bc25dc804700e1
+public class Draft_Task1_3_5_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Задание: \n6.  Пользователь вводит число, любое. 237, например. " +
+                "\nВыведите: 237 программистов, окончание должно зависеть от числа. 1 программист, " +
+                "\n2 программиста, и так далее\n\nРешение: ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        StringBuilder builder = new StringBuilder(scanner.nextLine());
+        System.out.println("Исходное число: " + builder);
+
+        int ost = Integer.parseInt(builder.toString()) % 100;
+        int ost2 = ost % 10;
+
+        StringBuilder builder1 = new StringBuilder(builder + " программист");
+        StringBuilder builder2 = new StringBuilder(builder + " программиста");
+        StringBuilder builder3 = new StringBuilder(builder + " программистов");
+
+        if (ost > 9 && ost < 20) {
+            System.out.println("Результат: " + builder3);
+        } else {
+            if (ost2 == 1) {
+                System.out.println("Результат: " + builder1);
+
+            } else if (ost2 > 1 && ost < 5) {
+                System.out.println("Результат: " + builder2);
+
+            } else {
+                System.out.println("Результат: " + builder3);
+
+            }
+        }
+    }
+}
+//        Конец Примера _ КККККККККККККККК
+
+
+////        Пример 9 ППППППППППППППППППППППППППППППППППП
 //public class Draft_Task1_3_5_6 {
 //    public static void main(String[] args) {
 //        System.out.println("Задание: \n6.  Пользователь вводит число, любое. 237, например. " +
@@ -19,63 +56,85 @@ import static java.lang.Math.min;
 ////        System.out.println("Итоговая строка: \"" + builderNew + "\"");
 //    }
 //}
-////        Конец Примера _ КККККККККККККККК
+////        Конец Примера 9 КККККККККККККККК
 
 
-
-
-
-
-//        Пример 8 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов
-//  Пример взят отсюда: https://gist.github.com/ezhov-da/1544cb56555f6319f5bc25dc804700e1
-public class Draft_Task1_3_5_6 {
-
-    public static void main(String[] args) {
-        System.out.println("Задание: \n6.  Пользователь вводит число, любое. 237, например. " +
-                "\nВыведите: 237 программистов, окончание должно зависеть от числа. 1 программист, " +
-                "\n2 программиста, и так далее\n\nРешение: ");
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        StringBuilder builder = new StringBuilder(scanner.nextLine());
-        System.out.println("Исходное число: " + builder);
-
-        String text = "%s %s";
-
-            System.out.println("Результат: " +
-                    String.format(text,
-                            builder,
-                            getName(builder, "программист", "программиста", "программистов"))
-            );
-    }
-
-    public static String getName(StringBuilder countOrigin, String firststr, String secondstr, String thirdstr) {
-        int count = Integer.parseInt(countOrigin.toString());
-        int ost = count % 100;
-        if (ost > 9 && ost < 20)
-        {
-            return thirdstr;
-        } else {
-            ost = ost % 10;
-            if (ost == 1)
-            {
-                return firststr;
-            } else if (ost > 1 && ost < 5)
-            {
-                return secondstr;
-            } else
-            {
-                return thirdstr;
-            }
-        }
-    }
-}
-//        Конец Примера 8 КККККККККККККККК
-
-
-
-
-
+////        Пример 8 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов
+////  Пример взят отсюда: https://gist.github.com/ezhov-da/1544cb56555f6319f5bc25dc804700e1
+//public class Draft_Task1_3_5_6 {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n6.  Пользователь вводит число, любое. 237, например. " +
+//                "\nВыведите: 237 программистов, окончание должно зависеть от числа. 1 программист, " +
+//                "\n2 программиста, и так далее\n\nРешение: ");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите число: ");
+//        StringBuilder builder = new StringBuilder(scanner.nextLine());
+//        System.out.println("Исходное число: " + builder);
+//
+//        int ost = Integer.parseInt(builder.toString()) % 100;
+//        int ost2 = ost % 10;
+//
+//        StringBuilder builder1 = new StringBuilder(builder.toString() + " программист");
+//        StringBuilder builder2 = new StringBuilder(builder.toString() + " программиста");
+//        StringBuilder builder3 = new StringBuilder(builder.toString() + " программистов");
+//
+//        if (ost > 9 && ost < 20) {
+////            return thirdstr;
+//            System.out.println("Результат: " + builder3);
+//        } else {
+////            ost = ost % 10;
+//            if (ost2 == 1) {
+////                return firststr;
+//                 System.out.println("Результат: " + builder1.toString());
+//
+//            } else if (ost2 > 1 && ost < 5) {
+////                return secondstr;
+//                System.out.println("Результат: " + builder2.toString());
+//
+//            } else {
+////                return thirdstr;
+//                System.out.println("Результат: " + builder3);
+//
+//            }
+//        }
+//
+//
+//
+//
+//
+////        String text = "%s %s";
+////
+////            System.out.println("Результат: " +
+////                    String.format(text,
+////                            builder,
+////                            getName(builder, "программист", "программиста", "программистов"))
+////            );
+//    }
+//
+////    public static String getName(StringBuilder countOrigin, String firststr, String secondstr, String thirdstr) {
+////        int count = Integer.parseInt(countOrigin.toString());
+////        int ost = count % 100;
+////        if (ost > 9 && ost < 20)
+////        {
+////            return thirdstr;
+////        } else {
+////            ost = ost % 10;
+////            if (ost == 1)
+////            {
+////                return firststr;
+////            } else if (ost > 1 && ost < 5)
+////            {
+////                return secondstr;
+////            } else
+////            {
+////                return thirdstr;
+////            }
+////        }
+////    }
+//}
+////        Конец Примера 8 КККККККККККККККК
 
 
 ////        Пример 7 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов _ Работает БЕЗ StringBuilder
@@ -123,11 +182,6 @@ public class Draft_Task1_3_5_6 {
 //    }
 //}
 ////        Конец Примера 7 КККККККККККККККК
-
-
-
-
-
 
 
 ////        Пример 6 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов
@@ -200,11 +254,6 @@ public class Draft_Task1_3_5_6 {
 ////        Конец Примера 6 КККККККККККККККК
 
 
-
-
-
-
-
 ////        Пример 5 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов _ ОРИГИНАЛ
 //public class Draft_Task1_3_5_6 {
 ////    private static final Logger LOG = Logger.getLogger(Draft_Task1_3_5_6.class.getName());
@@ -265,11 +314,6 @@ public class Draft_Task1_3_5_6 {
 //    }
 //}
 ////        Конец Примера 5 КККККККККККККККК
-
-
-
-
-
 
 
 ////        Пример 4 ППППППППППППППППППППППППППППППППППП
@@ -333,11 +377,6 @@ public class Draft_Task1_3_5_6 {
 ////        Конец Примера 4 КККККККККККККККК
 
 
-
-
-
-
-
 ////        Пример 3 ППППППППППППППППППППППППППППППППППП
 //public class Draft_Task1_3_5_6 {
 //    public static void main(String[] args) throws IOException, URISyntaxException, AWTException {
@@ -392,11 +431,6 @@ public class Draft_Task1_3_5_6 {
 //
 //
 ////        Конец Примера 3 КККККККККККККККК
-
-
-
-
-
 
 
 ////        Пример 2 ППППППППППППППППППППППППППППППППППП осталось - 14 - символов
@@ -455,10 +489,6 @@ public class Draft_Task1_3_5_6 {
 //    }
 //}
 ////        Конец Примера 2 КККККККККККККККК
-
-
-
-
 
 
 ////        Пример 1 ППППППППППППППППППППППППППППППППППП
