5b95bbe79f894f8ccd2812651b8fe0473cded80b
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-25 20:26:00 +0300

    Initial commit

--- src/Draft_Task1_3_5_11.java
+++ src/Draft_Task1_3_5_11.java
@@ -1,39 +1,71 @@
 import java.lang.reflect.*;
+import java.util.Scanner;
 
 import static sun.awt.image.PixelConverter.UshortGray.instance;
 
 //        Пример _ ППППППППППППППППППППППППППППППППППП
+//  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
 public class Draft_Task1_3_5_11 {
     public static void main(String[] args) {
+        System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation2, " +
+                "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
+                "\nсоздайте экземпляр класса.\n\nРешение: ");
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Пользователь, введите три переменных: ");
+
+//        System.out.print("Введите второе слово в качестве строкового поля классв FileInformation2: ");
+//        String word = scanner.next();
+
+
+//        String inputStr = scanner.nextLine();
+//        System.out.println("Исходные три переменных String: " + inputStr);
+
         CreateInstanceFromString createInstanceFromString = new CreateInstanceFromString();
         createInstanceFromString.cif();
-        try {
-            String className = "java.util.ArrayList";
-            Class<?> clazz = Class.forName(className);
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-            Method addMethod = clazz.getMethod("add", Object.class);
-            addMethod.invoke(instance, "Hello, world!");
-            System.out.println("Instance: " + instance);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod = clazz.getMethod("add", Object.class);
+//            addMethod.invoke(instance, "Hello, world!");
+////            System.out.println("Instance: " + instance);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
 
 class CreateInstanceFromString {
     static void cif() {
+        // Как пример, вводим три переменных
+        String str1, str2, str3;
+
+        str1 = "Объект 1";
+        str2 = "Объект 2";
+        str3 = "Объект 3";
         try {
             String className = "java.util.ArrayList";
             Class<?> clazz = Class.forName(className);
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-            Method addMethod = clazz.getMethod("add", Object.class);
-            addMethod.invoke(instance, "Hello, world!");
-            System.out.println("Instance: " + instance);
+            Object instance1 = clazz.getDeclaredConstructor().newInstance();
+            Object instance2 = clazz.getDeclaredConstructor().newInstance();
+            Object instance3 = clazz.getDeclaredConstructor().newInstance();
+            Method addMethod1 = clazz.getMethod("add", Object.class);
+            Method addMethod2 = clazz.getMethod("add", Object.class);
+            Method addMethod3 = clazz.getMethod("add", Object.class);
+            addMethod1.invoke(instance1, "Экземпляр класса 1");
+            System.out.println("Instance1: " + instance1);
+            addMethod2.invoke(instance2, "Экземпляр класса 2");
+            System.out.println("Instance2: " + instance2);
+            addMethod3.invoke(instance3, "Экземпляр класса 3");
+            System.out.println("Instance3: " + instance3);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    }
+}
 //        Конец Примера _ КККККККККККККККК
 
 
@@ -42,6 +74,133 @@ class CreateInstanceFromString {
 
 
 
+
+////        Пример 7 ППППППППППППППППППППППППППППППППППП
+////  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
+//public class Draft_Task1_3_5_11 {
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation2, " +
+//                "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
+//                "\nсоздайте экземпляр класса.\n\nРешение: ");
+//
+////        Scanner scanner = new Scanner(System.in);
+////        System.out.print("Пользователь, введите три переменных: ");
+//
+////        System.out.print("Введите второе слово в качестве строкового поля классв FileInformation2: ");
+////        String word = scanner.next();
+//
+//
+////        String inputStr = scanner.nextLine();
+////        System.out.println("Исходные три переменных String: " + inputStr);
+//
+//        CreateInstanceFromString createInstanceFromString = new CreateInstanceFromString();
+//        createInstanceFromString.cif();
+//
+////        try {
+////            String className = "java.util.ArrayList";
+////            Class<?> clazz = Class.forName(className);
+////            Object instance = clazz.getDeclaredConstructor().newInstance();
+////            Method addMethod = clazz.getMethod("add", Object.class);
+////            addMethod.invoke(instance, "Hello, world!");
+//////            System.out.println("Instance: " + instance);
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+//    }
+//}
+//
+//class CreateInstanceFromString {
+//    static void cif() {
+//        // Как пример, вводим три переменных
+//        String str1, str2, str3;
+//
+//        str1 = "Объект 1";
+//        str2 = "Объект 2";
+//        str3 = "Объект 3";
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance1 = clazz.getDeclaredConstructor().newInstance();
+//            Object instance2 = clazz.getDeclaredConstructor().newInstance();
+//            Object instance3 = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod1 = clazz.getMethod("add", Object.class);
+//            Method addMethod2 = clazz.getMethod("add", Object.class);
+//            Method addMethod3 = clazz.getMethod("add", Object.class);
+//            addMethod1.invoke(instance1, "Экземпляр класса 1");
+//            System.out.println("Instance1: " + instance1);
+//            addMethod2.invoke(instance2, "Экземпляр класса 2");
+//            System.out.println("Instance2: " + instance2);
+//            addMethod3.invoke(instance3, "Экземпляр класса 3");
+//            System.out.println("Instance3: " + instance3);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+////        Конец Примера 7 КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 6 ППППППППППППППППППППППППППППППППППП РАБОТАЕТ ПРИМЕР ИЗ ИСТОЧНИКА, ЕГО БЕРУ ЗА ОСНОВУ
+////  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
+//public class Draft_Task1_3_5_11 {
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation2, " +
+//                "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
+//                "\nсоздайте экземпляр класса.\n\nРешение: ");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Пользователь, введите три переменных: ");
+////        System.out.print("Введите второе слово в качестве строкового поля классв FileInformation2: ");
+////        String word = scanner.next();
+//
+//        // Как пример, вводим три этих переменных
+//        String str1, str2, str3;
+//
+//        str1 = "Объект 1";
+//        str2 = "Объект 2";
+//        str3 = "Объект 3";
+//
+//
+////        String inputStr = scanner.nextLine();
+////        System.out.println("Исходные три переменных String: " + inputStr);
+//
+//        CreateInstanceFromString createInstanceFromString = new CreateInstanceFromString();
+//        createInstanceFromString.cif();
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod = clazz.getMethod("add", Object.class);
+//            addMethod.invoke(instance, "Hello, world!");
+////            System.out.println("Instance: " + instance);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+//
+//class CreateInstanceFromString {
+//    static void cif() {
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod = clazz.getMethod("add", Object.class);
+//            addMethod.invoke(instance, "Hello, world!");
+//            System.out.println("Instance: " + instance);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    }
+////        Конец Примера 6 КККККККККККККККК
 
 
 ////        Пример 5 ППППППППППППППППППППППППППППППППППП
@@ -78,12 +237,6 @@ class CreateInstanceFromString {
 ////        Конец Примера 5 КККККККККККККККК
 
 
-
-
-
-
-
-
 ////        Пример 4 ППППППППППППППППППППППППППППППППППП Брать как исходный пример
 //public class Draft_Task1_3_5_11 {
 //    public static void main(String[] args) {
@@ -96,12 +249,6 @@ class CreateInstanceFromString {
 //    }
 //}
 ////        Конец Примера 4 КККККККККККККККК
-
-
-
-
-
-
 
 
 ////        Пример 3 ППППППППППППППППППППППППППППППППППП
@@ -284,10 +431,6 @@ class CreateInstanceFromString {
 //}//        Конец Примера 3 КККККККККККККККК
 
 
-
-
-
-
 ////        Пример 2 ППППППППППППППППППППППППППППППППППП
 ////  Пример с классом FileInformation2 взят отсюда:
 ////  видео мин 16 57  Урок 1. Введение в классы. Поля класса. Процедурное программирование, ООП,
@@ -322,10 +465,6 @@ class CreateInstanceFromString {
 //    }
 //}
 ////        Конец Примера 2 КККККККККККККККК
-
-
-
-
 
 
 ////        Пример 1 ППППППППППППППППППППППППППППППППППП
