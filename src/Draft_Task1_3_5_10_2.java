dba8caeab562dcf88c48758c94dcadb00674f3ce
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-26 21:10:55 +0300

    Initial commit

--- src/Draft_Task1_3_5_10_2.java
+++ src/Draft_Task1_3_5_10_2.java
@@ -27,3 +27,45 @@ public class Draft_Task1_3_5_10_2 {
 
 }
 //        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 1 ППППППППППППППППППППППППППППППППППП  Test 230726
+////  Пример взят отсюда: https://gb.ru/posts/java_ticktacktoe_game
+//public class Draft_Task1_3_5_10_2 {
+//    final char SIGN_X = 'x';
+//    final char SIGN_O = 'o';
+//    final char SIGN_EMPTY = '.';
+//    char[][] table;
+//    Random random;
+//    Scanner scanner;
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n10.  Напишите крестики-нолики, используя StringBuilder\n\nРешение: ");
+//
+//        new Draft_Task1_3_5_10_2().game();
+//    }
+//    Draft_Task1_3_5_10_2() {
+//        // конструктор: инициализация полей
+//    }
+//
+//    void game() {
+//        // игровая логика
+//    }
+//
+//    // дополнительные методы
+//
+//}
+////        Конец Примера 1 КККККККККККККККК
+
+
+
+
+
+
+
+
