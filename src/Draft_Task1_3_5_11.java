4b6324718eb61a6bd805765084e770b2abee1221
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-26 11:27:30 +0300

    Initial commit

--- src/Draft_Task1_3_5_11.java
+++ src/Draft_Task1_3_5_11.java
@@ -1,106 +1,213 @@
-import java.lang.reflect.*;
+import java.util.Scanner;
 
 //        Пример _ ППППППППППППППППППППППППППППППППППП
 //  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
 
 //  Создание класса и объекта Гоша Дударь: https://www.youtube.com/watch?v=_GLnOwDEE_A&list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK&index=13
 //  Создание класса и объекта selfedu: https://www.youtube.com/watch?v=eL4tJFvaH2g
+//  Здесь видео мин 08 38 ВАЖНО! Как sout в качестве объекта класса в main класс вывести
+//  https://www.youtube.com/watch?v=_GLnOwDEE_A&list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK&index=14
 public class Draft_Task1_3_5_11 {
     public static void main(String[] args) {
         System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation, " +
                 "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
                 "\nсоздайте экземпляр класса.\n\nРешение: ");
 
-        System.out.println("Создаем строку с тремя переменными: 'String str1, str2, str3;'");
-        System.out.println("Создаем три экземпляра класса FileInformation: ");
+        FileInformation scanStrings;
+        scanStrings = new FileInformation();
+        scanStrings.getScanner();
+        FileInformation getObjects;
+        getObjects = new FileInformation();
+        getObjects.getInstances();
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Пользователь, введите три переменных: ");
-
-//        System.out.print("Введите второе слово в качестве строкового поля классв FileInformation: ");
-//        String word = scanner.next();
-
-
-//        String inputStr = scanner.nextLine();
-//        System.out.println("Исходные три переменных String: " + inputStr);
-
-        FileInformation createIFS;
-//        createIFS.cif = new FileInformation();
-        createIFS = new FileInformation();
-        createIFS.fileInfo1();
-        createIFS.fileInfo2();
-        createIFS.fileInfo3();
-
-//        try {
-//            String className = "java.util.ArrayList";
-//            Class<?> clazz = Class.forName(className);
-//            Object instance = clazz.getDeclaredConstructor().newInstance();
-//            Method addMethod = clazz.getMethod("add", Object.class);
-//            addMethod.invoke(instance, "Hello, world!");
-////            System.out.println("Instance: " + instance);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 }
 
 class FileInformation {
-    static void fileInfo1() {
-        // Как пример, вводим три переменных
-        String str1, str2, str3;
+    public static Scanner getScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите через пробел, в качестве переменных (или полей класса) String, три слова, например " +
+                "\"Один\", \"Два\", \"Три\", \nзакончите ввод нажатием клавиши Enter: ");
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+        String str3 = scanner.next();
 
-        str1 = "Объект 1";
-        try {
-            String className = "java.util.ArrayList";
-            Class<?> clazz = Class.forName(className);
-            Object instance1 = clazz.getDeclaredConstructor().newInstance();
-            Method addMethod1 = clazz.getMethod("add", Object.class);
-            addMethod1.invoke(instance1, "Экземпляр класса 1");
-            System.out.println("Instance1: " + instance1);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        getInstances(str1, str2, str3);
+        return scanner;
+}
+    public static void getInstances(String str1, String str2, String str3) {
+        System.out.println();
+        System.out.println("Используя три введенных переменных, создаем экземпляр класса FileInformation и " +
+                "обращаемся к этим трём переменным, \nимя экземпляра класса \"getInstances\": ");
+        String object1 = str1;
+        String object2 = str2;
+        String object3 = str3;
+        System.out.println("Экземпляр класса 1: " + object1 + "\nЭкземпляр класса 2: " + object2 +
+                "\nЭкземпляр класса 3: " + object3);
     }
 
-    static void fileInfo2() {
-        // Как пример, вводим три переменных
-        String str1, str2, str3;
-
-        str2 = "Объект 2";
-        try {
-            String className = "java.util.ArrayList";
-            Class<?> clazz = Class.forName(className);
-            Object instance2 = clazz.getDeclaredConstructor().newInstance();
-            Method addMethod2 = clazz.getMethod("add", Object.class);
-            addMethod2.invoke(instance2, "Экземпляр класса 2");
-            System.out.println("Instance2: " + instance2);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void getInstances() {
     }
-
-    static void fileInfo3() {
-        // Как пример, вводим три переменных
-        String str1, str2, str3;
-
-        str3 = "Объект 3";
-        try {
-            String className = "java.util.ArrayList";
-            Class<?> clazz = Class.forName(className);
-            Object instance3 = clazz.getDeclaredConstructor().newInstance();
-            Method addMethod3 = clazz.getMethod("add", Object.class);
-            addMethod3.invoke(instance3, "Экземпляр класса 3");
-            System.out.println("Instance3: " + instance3);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
 //        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 9 ППППППППППППППППППППППППППППППППППП
+////  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
+//
+////  Создание класса и объекта Гоша Дударь: https://www.youtube.com/watch?v=_GLnOwDEE_A&list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK&index=13
+////  Создание класса и объекта selfedu: https://www.youtube.com/watch?v=eL4tJFvaH2g
+////  Здесь видео мин 08 38 ВАЖНО! Как sout в качестве объекта класса в main класс вывести
+////  https://www.youtube.com/watch?v=_GLnOwDEE_A&list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK&index=14
+//public class Draft_Task1_3_5_11 {
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation, " +
+//                "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
+//                "\nсоздайте экземпляр класса.\n\nРешение: ");
+//
+//        FileInformation scanStrings;
+//        scanStrings = new FileInformation();
+//        scanStrings.getScanner();
+//
+//    }
+//}
+//
+//class FileInformation {
+//    public static Scanner getScanner() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите через пробел, в качестве переменных String, три слова, например " +
+//                "\"Один\", \"Два\", \"Три\", \nзакончите ввод нажатием клавиши Enter: ");
+//        String str1 = scanner.next();
+//        String str2 = scanner.next();
+//        String str3 = scanner.next();
+//
+//        System.out.println();
+//        System.out.println("Используя три введенных переменных, создаем экземпляр класса FileInformation и обращаемся к этим трём переменным: ");
+//        String object1 = scanner.next();
+//        String object2 = scanner.next();
+//        String object3 = scanner.next();
+//        System.out.println("Экземпляр класса 1: " + object1 + "\nЭкземпляр класса 2: " + object2 +
+//                "\nЭкземпляр класса 3: " + object3);
+//        return scanner;
+//}
+//
+//}
+////        Конец Примера 9 КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 8 ППППППППППППППППППППППППППППППППППП
+////  Пример взят отсюда: https://www.sourcetrail.com/ru/java/java-get-class-by-string/
+//
+////  Создание класса и объекта Гоша Дударь: https://www.youtube.com/watch?v=_GLnOwDEE_A&list=PL0lO_mIqDDFW2xXiWSfjT7hEdOUZHVNbK&index=13
+////  Создание класса и объекта selfedu: https://www.youtube.com/watch?v=eL4tJFvaH2g
+//public class Draft_Task1_3_5_11 {
+//    public static void main(String[] args) {
+//        System.out.println("Задание: \n11.  Пусть пользователь вводит поля класса FileInformation, " +
+//                "\nно в одну строку; формат придумайте сами. Считайте ввод пользователя и из него " +
+//                "\nсоздайте экземпляр класса.\n\nРешение: ");
+//
+//        System.out.println("Создаем строку с тремя переменными: 'String str1, str2, str3;'");
+//        System.out.println("Создаем три экземпляра класса FileInformation: ");
+//
+////        Scanner scanner = new Scanner(System.in);
+////        System.out.print("Пользователь, введите три переменных: ");
+//
+////        System.out.print("Введите второе слово в качестве строкового поля классв FileInformation: ");
+////        String word = scanner.next();
+//
+//
+////        String inputStr = scanner.nextLine();
+////        System.out.println("Исходные три переменных String: " + inputStr);
+//
+//        FileInformation createIFS;
+////        createIFS.cif = new FileInformation();
+//        createIFS = new FileInformation();
+//        createIFS.fileInfo1();
+//        createIFS.fileInfo2();
+//        createIFS.fileInfo3();
+//
+////        try {
+////            String className = "java.util.ArrayList";
+////            Class<?> clazz = Class.forName(className);
+////            Object instance = clazz.getDeclaredConstructor().newInstance();
+////            Method addMethod = clazz.getMethod("add", Object.class);
+////            addMethod.invoke(instance, "Hello, world!");
+//////            System.out.println("Instance: " + instance);
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+//    }
+//}
+//
+//class FileInformation {
+//    static void fileInfo1() {
+//        // Как пример, вводим три переменных
+//        String str1, str2, str3;
+//
+//        str1 = "Объект 1";
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance1 = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod1 = clazz.getMethod("add", Object.class);
+//            addMethod1.invoke(instance1, "Экземпляр класса 1");
+//            System.out.println("Instance1: " + instance1);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    static void fileInfo2() {
+//        // Как пример, вводим три переменных
+//        String str1, str2, str3;
+//
+//        str2 = "Объект 2";
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance2 = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod2 = clazz.getMethod("add", Object.class);
+//            addMethod2.invoke(instance2, "Экземпляр класса 2");
+//            System.out.println("Instance2: " + instance2);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    static void fileInfo3() {
+//        // Как пример, вводим три переменных
+//        String str1, str2, str3;
+//
+//        str3 = "Объект 3";
+//        try {
+//            String className = "java.util.ArrayList";
+//            Class<?> clazz = Class.forName(className);
+//            Object instance3 = clazz.getDeclaredConstructor().newInstance();
+//            Method addMethod3 = clazz.getMethod("add", Object.class);
+//            addMethod3.invoke(instance3, "Экземпляр класса 3");
+//            System.out.println("Instance3: " + instance3);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
+////        Конец Примера 8 КККККККККККККККК
 
 
 
