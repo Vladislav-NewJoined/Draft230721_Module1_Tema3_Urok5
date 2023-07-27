ac433c1dbbf4187b521ffa923b6a55964b76de25
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-27 21:01:15 +0300

    Initial commit

---
+++ src/Draft_Task1_3_5_8.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+//        Пример _ ППППППППППППППППППППППППППППППППППП
+// Инфо здесь: Модуль 1, Тема 1, Урок 6. Как найти анализ курса валют за определенную дату. Урок 6 Видео мин 0.44.56
+// https://lms.synergy.ru/student/updiscipline/4474947/1045153/1/1
+public class Draft_Task1_3_5_8 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Задание: \n8. Вернитесь к программе, которая запрашивает курс валют. Напишите " +
+                "генератор даты для запроса к апи, на основе даты, введенной пользователем. " +
+                "ИспользуйтеStringBuilder." +
+                "\n\nРешение: ");
+    }
+}
+//        Конец Примера _ ККККККККККККККК
+
+
+
+
+
+
+
+
+
